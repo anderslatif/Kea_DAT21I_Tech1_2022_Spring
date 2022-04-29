f3abd6771e48fd54244512a0111f2b9187e8807c
Anders Latif <andl@kea.dk>
2022-04-29 09:58:32 +0200

    Added pages to Kea Shop

--- 6._Kea_Shop/src/main/java/com/example/kea_shop/ViewController/PagesController.java
+++ 6._Kea_Shop/src/main/java/com/example/kea_shop/ViewController/PagesController.java
@@ -3,14 +3,34 @@ package com.example.kea_shop.ViewController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class PagesController {
 
     @GetMapping("/")
     public String frontPage(Model model) {
+        ArrayList<String> features = new ArrayList<>();
+        features.add("You can see products");
+        features.add("You can contact us");
+        features.add("You can purchase things");
+        model.addAttribute("features", features);
+
         model.addAttribute("webshopName", "Kea Shop");
-        return "index";
+
+        return "frontpage/index";
+    }
+
+    @GetMapping("/products")
+    public String productsPage(Model model) {
+        return "products/products";
+    }
+
+    @GetMapping("/contact")
+    public String contactPage() {
+        return "contact/contact";
     }
 
 }
---
+++ 6._Kea_Shop/src/main/resources/sql/schema.sql
@@ -0,0 +1,7 @@
+/* todo Create a table that contains the shop name only */
+
+CREATE TABLE IF NOT EXISTS shop_name (
+    shop_name VARCHAR(100)
+);
+
+INSERT INTO shop_name VALUES ('Kea Shop');
